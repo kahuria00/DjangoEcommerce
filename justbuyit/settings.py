235ca0a83a2bd474f4da98ab0546e9aefaa0b578
k@huria00 <charitykahuria@gmail.com>
2021-05-04 06:03:43 +0300

    edited static file path/customize homepage

--- justbuyit/settings.py
+++ justbuyit/settings.py
@@ -57,7 +57,7 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_files')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -87,5 +87,3 @@ AUTHENTICATION_BACKENDS = [
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
 SITE_ID = 1
-
-
